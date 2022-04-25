99aa5ab7300c329575e73f6ae512ffd7f5c669c3
Aditya <likeadityakumar@gmail.com>
2022-04-25 17:57:48 +0530

    Time: 1 ms (95.60%), Space: 50.9 MB (38.28%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,6 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int s = 0;
+        //XOR any no with itself will give 0
+        // XOR all no then every duplicate no will yeild zero except single one
         for(int i = 0; i<nums.length; i++){
             s = s ^ nums[i];
         }
