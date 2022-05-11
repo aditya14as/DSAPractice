47414248a5676505ca543f3bbe50942e526a6d17
Aditya <likeadityakumar@gmail.com>
2022-05-11 17:40:28 +0530

    Time: 20 ms (85.89%), Space: 45.2 MB (85.39%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,15 +1,21 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int ans =0;
+        int sum =0;
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        // We need to add zero frequency as 1 because it will give zero ans if sum -k = 0 occurs at first time
+        map.put(0,1);
         for(int i=0; i<nums.length; i++){
-            int sum = 0;
-            for(int j=i; j<nums.length; j++){
-                sum = sum + nums[j];
-                if(sum==k) ans++;
+            sum = sum+ nums[i];
+            
+            //if map contains sum-k. it means total no of items passed between sum and sum-k indexes has sum k.
+            if(map.containsKey(sum-k)){
+                ans=ans+map.get(sum-k);
             }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        
-        
         return ans;
     }
 }
