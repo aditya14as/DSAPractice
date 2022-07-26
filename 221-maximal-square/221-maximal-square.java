9e60443ba3db7e9ada560fce9be4a520a7cb64df
Aditya <likeadityakumar@gmail.com>
2022-07-26 21:43:29 +0530

    Time: 33 ms (11.86%), Space: 77.1 MB (5.04%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,9 +1,10 @@
 class Solution {
+    int[][] dp = new int[300][300];
     public int maximalSquare(char[][] matrix) {
         if(matrix.length==0){
             return 0;
         }
-        int[][] dp = new int[matrix.length][matrix[0].length];
+        
         for(int i=0; i<dp.length; i++){
             Arrays.fill(dp[i],-1);
         }
@@ -12,19 +13,20 @@ class Solution {
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j]!='0'){
-                    int inner = helper(matrix,i,j,dp);
+                    int inner = helper(matrix,i,j);
                     curr = Math.max(curr,inner);
                 }
             }
         }
+        System.out.println(dp[1][2]);
         return curr*curr;
     }
-    public int helper(char[][] matrix, int i, int j,int[][] dp){
+    public int helper(char[][] matrix, int i, int j){
         if(i<0 || j<0 || i>=matrix.length || j>=matrix[0].length || matrix[i][j]=='0'){
             return 0;
         }
          if(dp[i][j] !=-1) return dp[i][j];
-        int ans = Math.min(helper(matrix,i+1,j,dp),Math.min(helper(matrix,i,j+1,dp),helper(matrix,i+1,j+1,dp)));
+        int ans = Math.min(helper(matrix,i+1,j),Math.min(helper(matrix,i,j+1),helper(matrix,i+1,j+1)));
         return dp[i][j] = ans+1;
     }
 }
