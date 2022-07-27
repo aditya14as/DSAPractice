2cad7f0a1e495418e83ac90209b14697f2f92d62
Aditya <likeadityakumar@gmail.com>
2022-07-27 18:14:08 +0530

    Time: 1 ms (82.69%), Space: 40.7 MB (74.62%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -1,31 +1,19 @@
 class Solution {
     public int minSteps(int n) {
-         int[][] dp = new int[n][n];
-        for(int i=0; i<dp.length; i++){
-            Arrays.fill(dp[i],-1);
+        int count = 0;
+        int buffer =0;
+        int current = 1;
+        while(current<n){
+            int rest = n-current;
+            if(rest%current==0){
+                buffer = current;
+                count=count+2;
+                current = current+buffer;
+            }else{
+                count++;
+                current = current+ buffer;
+            }
         }
-        return helper(n,0,1,dp);
-    }
-    public int helper(int n, int prev,int curr,int[][] dp){
-        if(n==curr){
-            return 0;
-        }
-        if(n<curr){
-            return 2147483000;
-        }
-        if(dp[curr][prev] != -1){
-            return dp[curr][prev];
-        }
-        if(prev==0){
-            //Initially we have to copy only
-             return dp[curr][prev]=1+helper(n,curr,curr,dp);
-        }
-        if(prev==curr){
-            //Here copy will only increase the operations so only pasting
-           return dp[curr][prev]= 1+helper(n,prev,prev+curr,dp);
-        }
-        int c = helper(n,curr,curr,dp);
-        int p = helper(n,prev,prev+curr,dp);
-        return dp[curr][prev]= 1+Math.min(c,p);
+        return count;
     }
 }
