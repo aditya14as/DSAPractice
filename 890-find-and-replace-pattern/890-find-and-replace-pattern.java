37a6d1bc156ee357d505bd002363af5cbf7d9905
Aditya <likeadityakumar@gmail.com>
2022-07-29 10:58:14 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (78.07%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<words.length; i++){
+            if(checkPattern(words[i],pattern)){
+                list.add(words[i]);
+            }
+        }
+        return list;
+    }
+    public boolean checkPattern(String word,String pattern){
+        for(int i=0; i<word.length();i++){
+            if(word.indexOf(word.charAt(i)) != pattern.indexOf(pattern.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
