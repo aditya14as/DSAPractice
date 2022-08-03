acdcd511c19870de63b321d3a2135a0509c5f484
Aditya <likeadityakumar@gmail.com>
2022-08-03 22:29:19 +0530

    Time: 41 ms (26.22%), Space: 81 MB (21.06%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,18 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        Integer[][] dp = new Integer[prices.length][2];
-        return helper(prices,fee,1,0,dp);
+        int[][] dp = new int[prices.length+1][2];
+        
+        for(int i=prices.length-1; i>=0; i--){
+            dp[i][1]=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][0]= Math.max(prices[i]-fee+dp[i+1][1],dp[i+1][0]);
+        }
+        return dp[0][1];
+        
+        // return helper(prices,fee,1,0,dp);
     }
-    public int helper(int[] prices,int fee,int canBuy, int i,Integer[][] dp){
-        if(i==prices.length){
-            return 0;
-        }
-        if(dp[i][canBuy]!=null){
-            return dp[i][canBuy];
-        }
-        if(canBuy==1){
-            return dp[i][canBuy]=Math.max(-prices[i]+helper(prices,fee,0,i+1,dp),helper(prices,fee,canBuy,i+1,dp));
-        }
-        return dp[i][canBuy] = Math.max(prices[i]-fee+helper(prices,fee,1,i+1,dp),helper(prices,fee,canBuy,i+1,dp));
-    }
+    // public int helper(int[] prices,int fee,int canBuy, int i,Integer[][] dp){
+    //     if(i==prices.length){
+    //         return 0;
+    //     }
+    //     if(dp[i][canBuy]!=null){
+    //         return dp[i][canBuy];
+    //     }
+    //     if(canBuy==1){
+    //         return dp[i][canBuy]=Math.max(-prices[i]+helper(prices,fee,0,i+1,dp),helper(prices,fee,canBuy,i+1,dp));
+    //     }
+    //     return dp[i][canBuy] = Math.max(prices[i]-fee+helper(prices,fee,1,i+1,dp),helper(prices,fee,canBuy,i+1,dp));
+    // }
 }
