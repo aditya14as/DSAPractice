6605073cd4dca5dbef023299308c5f40ba1c0ba0
Aditya <likeadityakumar@gmail.com>
2022-08-03 18:44:32 +0530

    Time: 1 ms (20.74%), Space: 41.2 MB (75.82%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,15 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
         Integer[] dp = new Integer[nums.length];
-        return helper(nums,0,dp);
-    }
-    public int helper(int[] nums,int i,Integer[] dp){
-        if(i>=nums.length){
-            return 0;
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2; i<nums.length; i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
         }
-        if(dp[i]!=null){
-            return dp[i];
-        }
-        return dp[i]=Math.max(nums[i]+helper(nums,i+2,dp),helper(nums,i+1,dp));
+        return dp[nums.length-1];
     }
+    // public int helper(int[] nums,int i,Integer[] dp){
+    //     if(i>=nums.length){
+    //         return 0;
+    //     }
+    //     if(dp[i]!=null){
+    //         return dp[i];
+    //     }
+    //     return dp[i]=Math.max(nums[i]+helper(nums,i+2,dp),helper(nums,i+1,dp));
+    // }
 }
