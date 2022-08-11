8285937b38cee5428d7a973035d50f2f6a39ca95
Aditya <likeadityakumar@gmail.com>
2022-08-11 17:31:04 +0530

    Time: 235 ms (13.16%), Space: 125.1 MB (18.31%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,19 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        Integer[][][] dp = new Integer[prices.length+1][2][3];
-        return helper(prices,0,1,2,dp);
+        int[][][] dp = new int[prices.length+1][2][3];
+        for(int i=prices.length-1; i>=0; i--){
+            for(int canBuy=0; canBuy<=1; canBuy++){
+                for(int cap=1; cap<=2; cap++){
+                    if(canBuy==1){
+                         dp[i][canBuy][cap]=Math.max(-prices[i]+dp[i+1][0][cap],dp[i+1][canBuy][cap]);
+                    }else{
+                         dp[i][canBuy][cap]=Math.max(prices[i]+dp[i+1][1][cap-1],dp[i+1][canBuy][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
-    public int helper(int[] prices, int i, int canBuy,int cap, Integer[][][] dp){
-        if(i==prices.length || cap==0){
-            return 0;
-        }
-        if(dp[i][canBuy][cap] !=null){
-            return dp[i][canBuy][cap];
-        }
-        if(canBuy==1){
-            return dp[i][canBuy][cap]=Math.max(-prices[i]+helper(prices,i+1,0,cap,dp),helper(prices,i+1,canBuy,cap,dp));
-        }else{
-            return dp[i][canBuy][cap]=Math.max(prices[i]+helper(prices,i+1,1,cap-1,dp),helper(prices,i+1,canBuy,cap,dp));
-        }
-    }
+    // public int helper(int[] prices, int i, int canBuy,int cap, Integer[][][] dp){
+    //     if(i==prices.length || cap==0){
+    //         return 0;
+    //     }
+    //     if(dp[i][canBuy][cap] !=null){
+    //         return dp[i][canBuy][cap];
+    //     }
+    //     if(canBuy==1){
+    //         return dp[i][canBuy][cap]=Math.max(-prices[i]+helper(prices,i+1,0,cap,dp),helper(prices,i+1,canBuy,cap,dp));
+    //     }else{
+    //         return dp[i][canBuy][cap]=Math.max(prices[i]+helper(prices,i+1,1,cap-1,dp),helper(prices,i+1,canBuy,cap,dp));
+    //     }
+    // }
 }
