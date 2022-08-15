5d11081dae5e82c5bc940f520630540911f55acc
Aditya <likeadityakumar@gmail.com>
2022-08-15 20:39:41 +0530

    Time: 24 ms (27.99%), Space: 44.1 MB (35.14%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int ans =0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int g=0; g<s.length();g++){
+            for(int i=0,j=g; j<s.length();j++,i++){
+                if(g==0){
+                    dp[i][j]=true;
+                }
+                else if(g==1){
+                    if(s.charAt(i)==s.charAt(j)){
+                        dp[i][j]=true;
+                    }else{
+                        dp[i][j]=false;
+                    }
+                }else{
+                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]){
+                        dp[i][j]=true;
+                    }else{
+                        dp[i][j]=false;
+                    }
+                }
+                if(dp[i][j]){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
