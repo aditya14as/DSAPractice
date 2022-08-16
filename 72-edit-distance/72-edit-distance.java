e05c7f13b3f146b3d9a048f36c8a500b0d956d1c
Aditya <likeadityakumar@gmail.com>
2022-08-16 11:31:42 +0530

    Time: 6 ms (91.44%), Space: 45.1 MB (42.29%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        Integer[][] dp = new Integer[word1.length()][word2.length()];
+        return helper(word1,word2,word1.length()-1,word2.length()-1,dp);
+    }
+    public int helper(String word1, String word2, int i, int j, Integer[][] dp){
+        if(i<0 && j<0){
+            return 0;
+        }
+        if(i<0 && j>=0){
+            return j+1;
+        }
+        if(i>=0 && j<0){
+            return i+1;
+        }
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j]=helper(word1,word2,i-1,j-1,dp);
+        }
+        int insert = 1+helper(word1,word2,i,j-1,dp);
+        int remove = 1 + helper(word1,word2,i-1,j,dp);
+        int replace = 1+ helper(word1,word2,i-1,j-1,dp);
+        return dp[i][j]=Math.min(insert,Math.min(remove,replace));
+    }
+}
