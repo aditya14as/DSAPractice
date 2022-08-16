89e6560d5df43692cadc5c4ed03ce2da64f8e91e
Aditya <likeadityakumar@gmail.com>
2022-08-16 10:41:14 +0530

    Time: 3 ms (25.85%), Space: 77.4 MB (18.37%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        //DP(On^2)
         // int[][] dp = new int[nums.length][nums.length];
         // int ans = Integer.MIN_VALUE;
         // for(int g=0; g<nums.length; g++){
@@ -14,14 +15,27 @@ class Solution {
         //     }
         // }
         // return ans;
-        int ans = Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=0; i<nums.length; i++){
-            if(sum<0){
-                sum=0;
-            }
-            sum += nums[i];
-            ans = Math.max(sum,ans);
+        
+        //Kadabe's Algorithm(O^n)
+        // int ans = Integer.MIN_VALUE;
+        // int sum=0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(sum<0){
+        //         sum=0;
+        //     }
+        //     sum += nums[i];
+        //     ans = Math.max(sum,ans);
+        // }
+        // return ans;
+        
+        //DP O(n)
+        
+        int[] dp = new int[nums.length];
+        int ans = nums[0];
+        dp[0]=nums[0];
+        for(int i=1; i<nums.length; i++){
+            dp[i]=Math.max(nums[i],dp[i-1]+nums[i]);
+            ans = Math.max(dp[i],ans);
         }
         return ans;
     }
