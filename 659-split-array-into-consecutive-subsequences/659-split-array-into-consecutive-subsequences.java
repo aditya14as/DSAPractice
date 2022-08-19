d0ea7919ffb2856b9251ea83e4f05d77147dce99
Aditya <likeadityakumar@gmail.com>
2022-08-19 13:00:06 +0530

    Time: 16 ms (79.77%), Space: 67.5 MB (74.77%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public boolean isPossible(int[] nums) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[1] == b[1]) {
+                    int la = a[1] - a[0] + 1;
+                    int lb = b[1] - b[0] + 1;
+                    return la - lb;
+                } else {
+                    return a[1] - b[1];
+                }
+            }
+        );
+        for (int i : nums) {
+            while (pq.size() > 0 && pq.peek()[1] + 1 < i) {
+                int[] r = pq.remove();
+                if(r[1]-r[0]+1<3){
+                    return false;
+                }
+            }
+            if(pq.size()==0 || pq.peek()[1]==i){
+                pq.add(new int[]{i,i});
+            }else{
+                int[] pr = pq.remove();
+                pq.add(new int[]{pr[0],i});
+            }
+        }
+        while(pq.size()>0){
+            int[] pr = pq.remove();
+            if(pr[1]-pr[0]+1 < 3){
+                return false;
+            }
+        }
+        return true;
+    }
+}
