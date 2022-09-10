4c5dbecc7f3cc67bef53bf4ceccda49a181613fa
Aditya <likeadityakumar@gmail.com>
2022-09-11 00:00:01 +0530

    Time: 6 ms (85.18%), Space: 63.8 MB (77.61%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,33 +1,42 @@
 class Solution {
-    public int makeConnected(int n, int[][] connections) {
-        if(connections.length<n-1){
+    int count=0;
+    public int makeConnected(int n, int[][] con) {
+        if(con.length<n-1){
             return -1;
         }
-        List<List<Integer>> adj = new ArrayList<>();
+        int[] rank = new int[n];
+        int[] parent = new int[n];
         for(int i=0; i<n; i++){
-            adj.add(new ArrayList<>());
+            rank[i]=0;
+            parent[i]=i;
         }
-        for(int i=0; i<connections.length; i++){
-            adj.get(connections[i][0]).add(connections[i][1]);
-            adj.get(connections[i][1]).add(connections[i][0]);
+        for(int i=0; i<con.length; i++){
+            int u = con[i][0];
+            int v = con[i][1];
+            union(parent,rank,u,v);
         }
-        boolean[] vis = new boolean[n];
-        int count = 0;
-        for(int i=0; i<n; i++){
-            
-            if(vis[i]==false){
-                count++;
-                getConnected(adj,i,vis);
-            }
-        }
-        return count-1;
+        return n-count-1;
     }
-    public void getConnected(List<List<Integer>> adj, int i, boolean[] vis){
-        vis[i]=true;
-        for(int j=0; j<adj.get(i).size(); j++){
-            if(vis[adj.get(i).get(j)]==false){
-                getConnected(adj,adj.get(i).get(j),vis);
+    public int findParent(int[] parent, int u){
+        if(parent[u]==u){
+            return u;
+        }
+        return parent[u]=findParent(parent,parent[u]);
+    }
+    public void union(int[] parent, int[] rank , int u , int v){
+        u = findParent(parent,u);
+        v = findParent(parent,v);
+        if(u!=v){
+            if(rank[u]<rank[v]){
+                parent[u]=v;
+            }else if(rank[v]<rank[u]){
+                parent[v]=u;
             }
+            else{
+                parent[u]=v;
+                rank[v]++;
+            }
+            count++;
         }
     }
 }
