79a72aeb205681a24004cedd8bc10e68cf43699c
Aditya <likeadityakumar@gmail.com>
2022-10-23 18:59:37 +0530

    Time: 16 ms (7.82%), Space: 42.8 MB (51.36%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,41 @@
+class Solution {
+    List<String> ans=new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        helper(digits,"");
+        return ans;
+    }
+    public void helper(String digits, String temp){
+        if(digits.length()==0){
+            if(temp.isEmpty()){
+                return;
+            }else{
+                ans.add(temp);
+                return;
+            }
+            
+        }
+        int digit = (digits.charAt(0)-'0')-1;
+        System.out.println(digit);
+        if(digit!=6 && digit!=7 && digit!=8){
+            for(int i = (digit-1)*3; i<digit*3; i++){
+                char ch = (char)('a'+i);
+                helper(digits.substring(1),temp+ch);
+            }
+        }else if(digit==6){
+            for(int i = (digit-1)*3; i<=digit*3; i++){
+                char ch = (char)('a'+i);
+                helper(digits.substring(1),temp+ch);
+            }
+        }else if(digit==7){
+            for(int i = (digit-1)*3+1; i<=digit*3; i++){
+                char ch = (char)('a'+i);
+                helper(digits.substring(1),temp+ch);
+            }
+        }else{
+            for(int i = (digit-1)*3+1; i<=digit*3+1; i++){
+                char ch = (char)('a'+i);
+                helper(digits.substring(1),temp+ch);
+            }
+        }
+    }
+}
