4e04041fd729ab9f6eb1ce5040f4d4b88929e232
Aditya <likeadityakumar@gmail.com>
2022-10-25 22:22:30 +0530

    Time: 7 ms (47.28%), Space: 67.3 MB (74.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        Boolean[] dp = new Boolean[nums.length+1];
-        return helper(nums,0,dp);
-    }
-    public boolean helper(int[] nums,int i, Boolean[] dp){
-        if(i>=nums.length-1){
+        if(nums.length==1){
             return true;
         }
-        if(dp[i]!=null){
-            return dp[i];
-        }
-        for(int j=nums[i]; j>0; j--){
-            if(helper(nums,j+i,dp)){
-                dp[i]=true;
-                return true;
+        int jump=nums[0];
+        if(jump==0) return false;
+        
+        for(int i=1; i<nums.length-1; i++){
+            jump--;
+            if(jump<nums[i]){
+                jump=nums[i];
+            }
+            if(jump<=0){
+                return false;
             }
         }
-        dp[i]=false;
-        return false;
+        return true;
     }
 }
