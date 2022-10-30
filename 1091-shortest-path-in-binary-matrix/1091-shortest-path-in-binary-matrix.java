ee7a3ee0ebacfd8b9a9651340d0dc6563921c47e
Aditya <likeadityakumar@gmail.com>
2022-10-30 19:30:21 +0530

    Time: 64 ms (23.09%), Space: 55.7 MB (80.78%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,42 +1,49 @@
 class Solution {
+
     public int shortestPathBinaryMatrix(int[][] grid) {
-        boolean[][] vis = new boolean[grid.length][grid[0].length];
-        if(grid[0][0]==1){
+        if (grid[0][0] == 1) {
             return -1;
         }
-        PriorityQueue<Pair> q = new PriorityQueue<>();
-        q.add(new Pair(0,0,0));
-        int[] drow = {0,1,1,1,0,-1,-1,-1};
-        int[] dcol = {1,1,0,-1,-1,-1,0,1};
-        while(q.size()>0){
-            Pair p = q.remove();
-            vis[p.row][p.col]=true;
-            for(int i=0; i<8; i++){
-                int nr = p.row+drow[i];
-                int nc = p.col+dcol[i];
-                if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && vis[nr][nc]==false && grid[nr][nc]==0){
-                    q.add(new Pair(nr,nc,p.dis+1));
-                    vis[nr][nc]=true;
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0, 0));
+        int[] drow = { 0, 1, 1, 1, 0, -1, -1, -1 };
+        int[] dcol = { 1, 1, 0, -1, -1, -1, 0, 1 };
+        while (pq.size() > 0) {
+            Pair p = pq.remove();
+            int row = p.row;
+            int col = p.col;
+            int dis = p.dis;
+            vis[row][col] = true;
+            for (int i = 0; i < 8; i++) {
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                int ndis = dis + 1;
+                if (nrow >= 0 && ncol >= 0 && nrow < grid.length && ncol < grid[0].length && vis[nrow][ncol] == false && grid[nrow][ncol] == 0) {
+                    pq.add(new Pair(nrow, ncol, ndis));
+                    vis[nrow][ncol]=true;
                 }
             }
-            if(p.row == grid.length-1 && p.col==grid[0].length-1){
-                return p.dis+1;
+            if (row == grid.length - 1 && col == grid[0].length - 1) {
+                return dis + 1;
             }
-            
         }
         return -1;
     }
 }
-class Pair implements Comparable<Pair>{
+
+class Pair implements Comparable<Pair> {
     int row;
     int col;
     int dis;
-    Pair(int row, int col, int dis){
+
+    Pair(int row, int col, int dis) {
         this.row = row;
         this.col = col;
         this.dis = dis;
     }
-    public int compareTo(Pair o){
-        return this.dis-o.dis;
+
+    public int compareTo(Pair o) {
+        return this.dis - o.dis;
     }
 }
